b193e04536b7cedf432a044445448c4bc54c715e
mateomejia401@gmail.com <juan.mejia-z@labinfo.is.escuelaing.edu.co>
2022-06-23 12:29:56 -0500

    Clase

---
+++ Clase06/target/classes/static/js/FirstComponent.jsx
@@ -0,0 +1,20 @@
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+   );
+
+function tick() {
+    const element = (
+    <div>
+        <h1>Hello, world xd!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+);
+ReactDOM.render(element, document.getElementById('rooot'));}
+setInterval(tick, 1000)
+
+ReactDOM.render(
+    <StatusComponent />,
+    document.getElementById('status')
+    );
+   
